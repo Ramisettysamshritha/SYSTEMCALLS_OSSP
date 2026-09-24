55e8880cc297d4d367c89521b087d871af809320
LAXMI SRI GUNDLA <laxmisrigundla@klh.edu.in>
2026-09-24 08:50:18 +0530

    Update zombie2.c

--- 2520030264_Practical/Pratical -4/zombie2.c
+++ 2520030264_Practical/Pratical -4/zombie2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
+int main() {
+    pid_t pid = fork();
+
+    if(pid == 0) {
+        printf("Child PID: %d\n", getpid());
+        exit(0);
+    }
+    else {
+        printf("Parent waiting...\n");
+        wait(NULL);
+        printf("Zombie removed. Child collected.\n");
+    }
+
+    return 0;
+}
