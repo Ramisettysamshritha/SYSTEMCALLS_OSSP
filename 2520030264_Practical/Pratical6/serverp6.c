90bc23ffcc31e8ffbc29132d868e8adf76fa494c
LAXMI SRI GUNDLA <laxmisrigundla@klh.edu.in>
2026-09-24 09:07:51 +0530

    Update serverp6.c

--- 2520030264_Practical/Pratical6/serverp6.c
+++ 2520030264_Practical/Pratical6/serverp6.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
 
+int main() {
+    char msg[100];
+    mkfifo("fifo1", 0666);
+    mkfifo("fifo2", 0666);
+
+    while(1) {
+        int fd1 = open("fifo1", O_RDONLY);
+        read(fd1, msg, sizeof(msg));
+        close(fd1);
+
+        printf("Client: %s\n", msg);
+
+        strcat(msg, " [Received by Server]");
+
+        int fd2 = open("fifo2", O_WRONLY);
+        write(fd2, msg, sizeof(msg));
+        close(fd2);
+    }
+
+    return 0;
+}
