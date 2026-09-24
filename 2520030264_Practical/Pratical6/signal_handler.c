eb1a1312c6b9cf1d6eeac440c76a4620a97b72e1
LAXMI SRI GUNDLA <laxmisrigundla@klh.edu.in>
2026-09-24 09:08:41 +0530

    Update signal_handler.c

--- 2520030264_Practical/Pratical6/signal_handler.c
+++ 2520030264_Practical/Pratical6/signal_handler.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+void handler(int sig) {
+    if(sig == SIGINT)
+        printf("\nSIGINT received (Ctrl+C)\n");
+
+    else if(sig == SIGTERM) {
+        printf("\nSIGTERM received\n");
+        exit(0);
+    }
+
+    else if(sig == SIGUSR1)
+        printf("\nSIGUSR1 received\n");
+}
+
+int main() {
+    signal(SIGINT, handler);
+    signal(SIGTERM, handler);
+    signal(SIGUSR1, handler);
+
+    printf("Process ID: %d\n", getpid());
+
+    while(1) {
+        printf("Running...\n");
+        sleep(3);
+    }
+
+    return 0;
+}
